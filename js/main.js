0adbb99eacaa377270fe6933d157465d5ddfa2e9
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-05-18 02:58:49 -0700

    Pages: Rewiring routing. Now no hardcoded routes. If folder exists, we honor route.
    Folder must contain "main.js" - AMD module that loads resources specific to that folder.

--- js/main.js
+++ js/main.js
@@ -2,95 +2,112 @@
  * Copyright 2012 Willow Systems Corporation (willow-systems.com)
 */
 
-;(function($){
+;(function(){
+'use strict'
+
+function PubSub(){
+    'use strict'
+    /**  @preserve 
+    -----------------------------------------------------------------------------------------------
+    2012 (c) ddotsenko@willowsystems.com
+    based on Peter Higgins (dante@dojotoolkit.org)
+    Loosely based on Dojo publish/subscribe API, limited in scope. Rewritten blindly.
+    Original is (c) Dojo Foundation 2004-2010. Released under either AFL or new BSD, see:
+    http://dojofoundation.org/license for more information.
+    -----------------------------------------------------------------------------------------------
+    */
+    this.topics = {};
+    /**
+     * Allows caller to emit an event and pass arguments to event listeners.
+     * @public
+     * @function
+     * @param topic {String} Name of the channel on which to voice this event
+     * @param **arguments Any number of arguments you want to pass to the listeners of this event.
+     */
+    this.publish = function(topic, arg1, arg2, etc) {
+        'use strict'
+        if (this.topics[topic]) {
+            var currentTopic = this.topics[topic]
+            , args = Array.prototype.slice.call(arguments, 1)
+            
+            for (var i = 0, l = currentTopic.length; i < l; i++) {
+                currentTopic[i].apply(null, args);
+            }
+        }
+    };
+    /**
+     * Allows listener code to subscribe to channel and be called when data is available 
+     * @public
+     * @function
+     * @param topic {String} Name of the channel on which to voice this event
+     * @param callback {Function} Executable (function pointer) that will be ran when event is voiced on this channel.
+     * @returns {Object} A token object that cen be used for unsubscribing.  
+     */
+    this.subscribe = function(topic, callback) {
+        'use strict'
+        if (!this.topics[topic]) {
+            this.topics[topic] = [callback];
+        } else {
+            this.topics[topic].push(callback);
+        }
+        return {
+            "topic": topic,
+            "callback": callback
+        };
+    };
+    /**
+     * Allows listener code to unsubscribe from a channel 
+     * @public
+     * @function
+     * @param token {Object} A token object that was returned by `subscribe` method 
+     */
+    this.unsubscribe = function(token) {
+        if (this.topics[token.topic]) {
+            var currentTopic = this.topics[token.topic]
+            
+            for (var i = 0, l = currentTopic.length; i < l; i++) {
+                if (currentTopic[i] === token.callback) {
+                    currentTopic.splice(i, 1)
+                }
+            }
+        }
+    };
+};
+
+require(
+	['jquery']
+	, function($){
 	"use strict"
 
-	/*  @preserve
-	jQuery pub/sub plugin 
-	2012 (c) ddotsenko@willowsystems.com
-	based on Peter Higgins (dante@dojotoolkit.org)
-	Loosely based on Dojo publish/subscribe API, limited in scope. Rewritten blindly.
-	Original is (c) Dojo Foundation 2004-2010. Released under either AFL or new BSD, see:
-	http://dojofoundation.org/license for more information.
-	*/
-	;(function($) {
-		var topics = {};
-		/**
-		 * Allows caller to emit an event and pass arguments to event listeners.
-		 * @public
-		 * @function
-		 * @param topic {String} Name of the channel on which to voice this event
-		 * @param **arguments Any number of arguments you want to pass to the listeners of this event.
-		 */
-		$.publish = function(topic, arg1, arg2, etc) {
-			if (topics[topic]) {
-				var currentTopic = topics[topic]
-				, args = Array.prototype.slice.call(arguments, 1)
-				
-				for (var i = 0, j = currentTopic.length; i < j; i++) {
-				    currentTopic[i].apply($, args);
-				}
-			}
-		};
-		/**
-		 * Allows listener code to subscribe to channel and be called when data is available 
-		 * @public
-		 * @function
-		 * @param topic {String} Name of the channel on which to voice this event
-		 * @param callback {Function} Executable (function pointer) that will be ran when event is voiced on this channel.
-		 * @returns {Object} A token object that cen be used for unsubscribing.  
-		 */
-		$.subscribe = function(topic, callback) {
-			if (!topics[topic]) {
-			    topics[topic] = [callback];
-			} else {
-				topics[topic].push(callback);		    
-			}
-			return {
-			    "topic": topic,
-			    "callback": callback
-			};
-		};
-		/**
-		 * Allows listener code to unsubscribe from a channel 
-		 * @public
-		 * @function
-		 * @param token {Object} A token object that was returned by `subscribe` method 
-		 */
-		$.unsubscribe = function(token) {
-			if (topics[token.topic]) {
-				var currentTopic = topics[token.topic];
-				
-				for (var i = 0, j = currentTopic.length; i < j; i++) {
-				    if (currentTopic[i] === token.callback) {
-				        currentTopic.splice(i, 1);
-				    }
-				}
-			}
-		};
-	})($);
-	// end of PubSub plugin
-	
 	define(
-		'jquery'
-		,function(){return $}
+		'pubsub'
+		, function() {return PubSub}
 	)
 
  	define(
- 		'requirements'
- 		, [
-			//'js/libs/jquery.inputs-20120113.min'
-			//, 'js!js/libs/jstorage-0.1.6.1.min.js'
-			'js!js/libs/sammy.min.js'
-			, 'js!js/libs/jSignature.min.js'
-		]
+ 		'sammyjs'
+ 		, ['js!js/libs/sammy.min.js']
  		, function() {return $}
+ 	)
+	
+ 	define(
+ 		'jsignature'
+ 		, ['js!js/libs/jSignature.min.js']
+ 		, function() {return $}
+ 	)
+
+ 	define(
+ 		'mustache'
+ 		, ['js!js/libs/mustache.min.js']
+ 		, function(){
+ 			return Mustache
+ 		}
  	)
  	
  	define(
  	    "myapp"
- 	    , ['jquery', 'js/router', 'requirements']
- 	    , function($, router) {
+ 	    , ['js/router', 'sammyjs', 'jsignature']
+ 	    , function(router) {
  	        // note, myapp_ may, but does not have to contain RequireJS-compatible define that returns something.
  	        // however, if it contains something like "$(document).ready(function() { ... " already it MAY fire before 
  	        // its depends - sammy, jquery plugins - are fully loaded.
@@ -98,17 +115,17 @@
  	        // that takes jQuery (with all loaded , applied plugins) 
  	        // The expectation is that before the below return is executed, all depends are loaded (in order of depends tree)
  	        // You could init your app here like so:
- 	        return router($) 
+ 	        return router 
  	        // catch the returned instance in require( and run the pre-inited application instance there
  	    }
  	)
 
-	require(['jquery', 'myapp'])
-	.then(function($, app) {
-		$(document).ready(function() {app.Run()})
-	})
-})(
-	typeof jQuery === "function" ? 
-	jQuery : 
-	function(){throw new Error("Where is jQuery?")}
-);
+	require(
+		['jquery', 'myapp']
+		,function($, Initializer) {
+			$(document).ready(function() {Initializer($).Run()})
+		}
+	)
+})
+
+}).call(typeof window === 'object' ? window : this )
--- js/router.js
+++ js/router.js
@@ -1,4 +1,3 @@
-define(function() {
 /* 
  * Sammy.js, PubSub-based router for jSignature project home page.
  *  
@@ -10,12 +9,71 @@ define(function() {
  * 
  * Copyright 2012 Willow Systems Corporation (willow-systems.com)
  */
+define(
+['jquery']
+, function($) {
 return function($) {
 	'use strict'
 	
 	var namespace = 'jSignatureHome'
 	, dynamicAppSelector = '#main_content_wrap'
-	
+		
+	// All of the class definition code expects that DOM IS NOT LOADED YET.
+	// everything is done through predefined closures and callbacks that WILL
+	// fire when app instance is finally ran.
+	var RouterClass = function($, namespace) {
+		var APP_NAME = this.APP_NAME = "jSignature Home Page"
+		    
+		var app = this 
+		
+		// This code sets up Sammy.js application. It is inited at the very end.
+		app.sammyapp = $.sammy(function() {
+		    this.raise_errors = false
+		    this.debug = false
+		    this.run_interval_every = 300
+		    this.template_engine = null
+		    this.element_selector = dynamicAppSelector
+		    // this.use('GoogleAnalytics')
+		})
+
+		function deriveRequireablePath(context, path){
+
+			var pathParts = path.split("/")
+			, notAFolder = pathParts.pop()
+			, folderName = pathParts.join('/')
+			, mainjsPath = folderName + '/' + 'main'
+			, urlPrefix = String(window.location.href).split(window.location.hash)[0]
+
+			require([urlPrefix + mainjsPath])
+			.then(
+				function(resource){
+					resource.call(context)
+				}
+				, function(resource){
+					// context.app.swap("404 - Not Found")
+					document.title = "404 - Not Found"
+					context.app.notFound()
+				} 
+			)
+		}
+		
+		// ROUTES Have to be defined from complex to simple. Otherwise the shortest match will pick it up first.
+		app.sammyapp.route('get', /#\/(.*)/, function() {
+			document.title = APP_NAME
+			deriveRequireablePath(this, this.params.splat[0])
+		})
+		
+		// ----------------------------------------------------------------------
+		
+		//------------------------------------------------------------------------
+		// !!! This is what parent code will pull when it's time for us to run !!!
+		//------------------------------------------------------------------------
+		app.Run = function() {
+		    this.sammyapp.run('#/about/')
+		    console.log(this.APP_NAME + " is running")
+		}
+	}
+
 	$(dynamicAppSelector).on('click.sammy_front_runner', 'a', function(e){
 		var $t = $(e.currentTarget)
 		, encodedHash = $t.attr('href')
@@ -46,169 +104,7 @@ return function($) {
 		}
 		return true;
 	})
-	
-	// All of the class definition code expects that DOM IS NOT LOADED YET.
-	// everything is done through predefined closures and callbacks that WILL
-	// fire when app instance is finally ran.
-	var RouterClass = function($, namespace) {
-		this.APP_NAME = "jSignature Home Page"
-		    
-		var app = this 
-		
-		// This code sets up Sammy.js application. It is inited at the very end.
-		app.sammyapp = $.sammy(function() {
-		    this.raise_errors = false
-		    this.debug = false
-		    this.run_interval_every = 300
-		    this.template_engine = null
-		    this.element_selector = dynamicAppSelector
-		    this.use('GoogleAnalytics')
-		})
-		
-		// ROUTES Have to be defined from complex to simple. Otherwise the shortest match will pick it up first.
-		
-		app.sammyapp.route('get',/\#\/PageNotFound\//, function() {
-			this.noTrack()
-			document.title = "404 - Page not found"
-		    $.publish(namespace + "_show_404_page")
-		    // this.notFound()
-		})
-		app.sammyapp.route('get',/\#\/demo\//, function() {
-			document.title = "jSignature Demo"
-			$.publish(namespace + "_show_demo_page")
-		})
-		app.sammyapp.route('get',/\#\/(.+)/, function() {
-			this.noTrack()
-			this.app.setLocation("#/PageNotFound/")
-		})
-		app.sammyapp.route('get',/\#\//, function() {
-			this.noTrack()
-			document.title = "About jSignature"
-		    $.publish(namespace + "_show_about_page")
-		})
-		app.sammyapp.route('get',/(.*)/, function() {
-			this.noTrack()
-			this.app.setLocation("#/PageNotFound/")
-		})
-		
-		// ----------------------------------------------------------------------
-		
-		app.setLocation = function(location){
-			app.sammyapp.setLocation(newpath)
-		}
-		
-		//------------------------------------------------------------------------
-		// !!! This is what parent code will pull when it's time for us to run !!!
-		//------------------------------------------------------------------------
-		app.Run = function() {
-		    this.sammyapp.run('#/')
-		    console.log(this.APP_NAME + " is running")
-		}
-	}
 
-	
-	$.subscribe(
-		namespace + "_show_about_page"
-		, function(){
-			$('#demo').hide()
-			$('#about').show()
-		}
-	)
-
-	$.subscribe(
-		namespace + "_show_demo_page"
-		, function(){
-			$('#about').hide()
-			
-            $('#demo').html('<div id="signatureparent">jSignature inherits colors from here - parent element<div id="signature"></div></div><div id="tools"></div><div><p>Display Area:</p><div id="displayarea"></div></div>').show()
-            
-            var $sigdiv = $('#signature').jSignature()
-        	, $tools = $('#tools')
-        	, $extraarea = $('#displayarea')
-        	, export_plugins = $sigdiv.jSignature('listPlugins','export')
-        	, chops = ['<span><b>Extract signature data as: </b></span><select>','<option value="">(select export format)</option>']
-        	, name
-        	, pubsubprefix = namespace
-
-        	for(var i in export_plugins){
-        		if (export_plugins.hasOwnProperty(i)){
-        			name = export_plugins[i]
-        			chops.push('<option value="' + name + '">' + name + '</option>')
-        		}
-        	}
-        	chops.push('</select><span><b> or </b></span>')
-        	
-        	$(chops.join('')).bind('change', function(e){
-        		if (e.target.value !== ''){
-        			var data = $sigdiv.jSignature('getData', e.target.value)
-        			$.publish(pubsubprefix + 'formatchanged')
-        			if (typeof data === 'string'){
-        				$('textarea', $tools).val(data)
-        			} else if($.isArray(data) && data.length === 2){
-        				$('textarea', $tools).val(data.join(','))
-        				$.publish(pubsubprefix + data[0], data);
-        			} else {
-        				try {
-        					$('textarea', $tools).val(JSON.stringify(data))
-        				} catch (ex) {
-        					$('textarea', $tools).val('Not sure how to stringify this, likely binary, format.')
-        				}
-        			}
-        		}
-        	}).appendTo($tools)
-
-        	
-        	$('<input type="button" value="Reset">').bind('click', function(e){
-        		$sigdiv.jSignature('reset')
-        	}).appendTo($tools)
-        	
-        	$('<div><textarea style="width:100%;height:7em;"></textarea></div>').appendTo($tools)
-        	
-        	$.subscribe(pubsubprefix + 'formatchanged', function(){
-        		$extraarea.html('')
-        	})
-
-        	$.subscribe(pubsubprefix + 'image/svg+xml', function(data) {
-        		var i = new Image()
-        		i.src = 'data:' + data[0] + ';base64,' + btoa( data[1] )
-        		$(i).appendTo($extraarea)
-        		
-        		var message = [
-        			"If you don't see an image immediately above, it means your browser is unable to display in-line (data-url-formatted) SVG."
-        			, "This is NOT an issue with jSignature, as we can export proper SVG document regardless of browser's ability to display it."
-        			, "Try this page in a modern browser to see the SVG on the page, or export data as plain SVG, save to disk as text file and view in any SVG-capabale viewer."
-                   ]
-        		$( "<div>" + message.join("<br/>") + "</div>" ).appendTo( $extraarea )
-        	});
-
-        	$.subscribe(pubsubprefix + 'image/svg+xml;base64', function(data) {
-        		var i = new Image()
-        		i.src = 'data:' + data[0] + ',' + data[1]
-        		$(i).appendTo($extraarea)
-        		
-        		var message = [
-        			"If you don't see an image immediately above, it means your browser is unable to display in-line (data-url-formatted) SVG."
-        			, "This is NOT an issue with jSignature, as we can export proper SVG document regardless of browser's ability to display it."
-        			, "Try this page in a modern browser to see the SVG on the page, or export data as plain SVG, save to disk as text file and view in any SVG-capabale viewer."
-                   ]
-        		$( "<div>" + message.join("<br/>") + "</div>" ).appendTo( $extraarea )
-        	});
-        	
-        	$.subscribe(pubsubprefix + 'image/png;base64', function(data) {
-        		var i = new Image()
-        		i.src = 'data:' + data[0] + ',' + data[1]
-        		$('<span><b>As you can see, one of the problems of "image" extraction (besides not working on some old Androids, elsewhere) is that it extracts A LOT OF DATA and includes all the decoration that is not part of the signature.</b></span>').appendTo($extraarea)
-        		$(i).appendTo($extraarea)
-        	});
-        	
-        	$.subscribe(pubsubprefix + 'image/jsignature;base30', function(data) {
-        		$('<span><b>This is a vector format not natively render-able by browsers. Format is a compressed "movement coordinates arrays" structure tuned for use server-side. The bonus of this format is its tiny storage footprint and ease of deriving rendering instructions in programmatic, iterative manner.</b></span>').appendTo($extraarea)
-        	});
-        	
-		}
-	)
-
-	
 	// 2. returning class's instance
 	return new RouterClass($, namespace) // notice that this is INITED app, but not started 
 	// .Run will be pulled by parent code when it is appropriate
