129c9138f815010b9b2bb19fc1923cd999e14ff3
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-08-16 15:43:27 -0700

    Pages: adding zip packager to build script

--- wscript.py
+++ wscript.py
@@ -1,6 +1,14 @@
 #! /usr/bin/env python
 # encoding: utf-8
 import subprocess
+import zipfile
+
+def default(context):
+    print("====== Importing Recent jSignature build")
+    importbuild(context)
+    packzip(context)
+    print("====== Exporting SEO-friendly versions of dynamic pages")
+    generateseopages(context)
 
 def importbuild(context):
 	subprocess.call(
@@ -12,6 +20,19 @@ def importbuild(context):
 		, shell = True
 	)
 
+def packzip(context):
+    files = [
+        'js/libs/jSignature.min.js'
+        , 'js/libs/flashcanvas.js'
+        , 'js/libs/flashcanvas.swf'
+    ]
+    with zipfile.ZipFile('jSignature.zip', 'w') as zfile:
+        for fn in files:
+            zfile.write(
+                fn
+                , context.Node(fn).name
+            )
+
 def generateseopages(context):
 	buildparts = context.Node('./buildparts/')
 	subprocess.call(
@@ -22,13 +43,6 @@ def generateseopages(context):
 		]
 		, shell = True
 	)	
-
-def default(context):
-	print("====== Importing Recent jSignature build")
-	importbuild(context)
-	print("====== Exporting SEO-friendly versions of dynamic pages")
-	generateseopages(context)
-
 
 if __name__ == "__main__":
 	print("This is a Wak (https://github.com/dvdotsenko/Wak) automation script file.\n(Export 'NOCLIMB' to shell scope and) run 'wak.py' against this folder.")
